5c20e23cd2481788bcefc6e9b60d651e20fd9fa2
Miguel Ferreira <miguelferreira@me.com>
2021-11-30 14:02:55 +0100

    Add action debug statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-set -e
+set -euo pipefail
+IFS=$(printf ' \n\t')
 
-export AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
+debug() {
+  if [ "${ACTIONS_RUNNER_DEBUG:-}" = "true" ]; then
+    echo "DEBUG: :: $*" >&2
+  fi
+}
+
+export AWS_ACCESS_KEY_ID="${INPUT_AWS_ACCESS_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${INPUT_AWS_SECRET_ACCESS_KEY}"
 
 echo "aws version"
 
@@ -11,5 +18,9 @@ aws --version
 
 echo "Attempting to update kubeconfig for aws"
 
-aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
-echo ::set-output name=kubectl-out::"$( kubectl "$@" )"
+aws eks --region "${INPUT_AWS_REGION}" update-kubeconfig --name "${INPUT_CLUSTER_NAME}"
+
+debug "Starting kubectl collecting output"
+output=$( kubectl "$@" )
+debug "${output}"
+echo ::set-output name=kubectl-out::"${output}"
